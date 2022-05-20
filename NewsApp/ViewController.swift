50e92b1bba9c427f7756e59159964d322eeb0baa
Sohel <absohel47@gmail.com>
2022-05-20 20:25:43 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Abdullah Al Sohel on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "News"
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
